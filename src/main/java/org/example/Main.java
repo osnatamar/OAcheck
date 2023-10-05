ad1949fbd73dcc632a969c143471781e76fa90b0
AmielCohen96 <117925491+AmielCohen96@users.noreply.github.com>
2023-10-05 15:36:33 +0300

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Osnat check");
+        System.out.println("Osnat check 2");
     }
 }
